502eca7ab41abc897bb85d24a8923d775ce0e3f9
geekyJock8 <vikas.rajput54@gmail.com>
2019-11-17 13:33:43 +0530

    Updates

---
+++ precache-manifest.8334f4adfba32c5203c900d006c16d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3ac712a930119655d1ce50d99bf4d8",
+    "url": "/wolf_classifier/index.html"
+  },
+  {
+    "revision": "fcf91b81cfe463ddfaf9",
+    "url": "/wolf_classifier/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "728900706fe91d9326f8",
+    "url": "/wolf_classifier/static/css/main.86a655b7.chunk.css"
+  },
+  {
+    "revision": "fcf91b81cfe463ddfaf9",
+    "url": "/wolf_classifier/static/js/2.9546dab0.chunk.js"
+  },
+  {
+    "revision": "728900706fe91d9326f8",
+    "url": "/wolf_classifier/static/js/main.fa735a33.chunk.js"
+  },
+  {
+    "revision": "5d7d030dc50e5874119f",
+    "url": "/wolf_classifier/static/js/runtime~main.f962cc77.js"
+  }
+]);
